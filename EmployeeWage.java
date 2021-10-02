2ae7a795ebcf58529fce6d38de1eccd35ff06bdc
Samim Aktar <samimaktar0773@gmail.com>
2021-10-02 17:44:39 +0530

    Done UC1: Check Emp is Present Or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,6 +3,21 @@ package sample;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		// initialize and declar a emp_is_present variable 
+		int emp_is_present=1;
+				
+		//take the random value
+		double checkEmp = Math.floor(Math.random()*10)%2;
+//		System.out.println("CheckEmp: "+checkEmp);
+
+		//check the codition emp is present or not 
+		if(checkEmp == emp_is_present)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+			System.out.println("Emplyee is Absent");
 	}
 
 }
