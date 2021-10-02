9f4cc205ff8a073b4364e483251097358108817c
Samim Aktar <samimaktar0773@gmail.com>
2021-10-02 18:03:43 +0530

    Done UC2: Calculate Daily Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,12 +4,14 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
+        //******************** UC:1 - Check Emp is present or absent ***************
+		
 		// initialize and declar a emp_is_present variable 
 		int emp_is_present=1;
 				
 		//take the random value
 		double checkEmp = Math.floor(Math.random()*10)%2;
-//		System.out.println("CheckEmp: "+checkEmp);
+        //		System.out.println("CheckEmp: "+checkEmp);
 
 		//check the codition emp is present or not 
 		if(checkEmp == emp_is_present)
@@ -18,6 +20,22 @@ public class EmployeeWage {
 		}
 		else
 			System.out.println("Emplyee is Absent");
+		
+		//************************ UC:2- Calculate Daily Emp Wage *********************************
+		
+		   // declar variable and initialize value
+			
+		   int Wage_Per_Hour = 20;
+		   int Full_Day_Hour = 8;
+					
+		   // calculate daily wage
+		   int total_Wage = Wage_Per_Hour * Full_Day_Hour;
+					
+		   //Print total Wage per Day
+		   System.out.println("Daily Employee Wage : " + total_Wage);
+		
+       
+		
 	}
 
 }
