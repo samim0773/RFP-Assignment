5b5f4e851b2b8ae23ff53fdd82ab5f9fd88098de
Samim Aktar <samimaktar0773@gmail.com>
2021-10-10 23:43:13 +0530

    Add comment

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,33 +2,46 @@ package sample;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
+		
+		// initialize variable and assign value
 		int emp_Hour;
 		final int is_Full_Time = 1;
 		final int is_Part_Time = 2;
 		int total_Salary = 0;
 		int working_Days = 20;
 		int emp_Rate_Per_Hour = 20;
-
+		
+		// execute for loop till upto  working day
 		for(int i=0; i<=working_Days; i++){
 			
+			// generate randome number 0 1 2
 			int emp_Check = (int) Math.floor(Math.random()*3);
-
+			
+			// check condition 
 			switch(emp_Check) {
+			
+				// full time = 1
 				case is_Full_Time:
 					emp_Hour = 8;
 					break;
+					
+				// part time = 2
 				case is_Part_Time:
 					emp_Hour = 4;
 					break;
+					
+				// absent = 0
 				default:
 					emp_Hour = 0;
 			}
 			
+			//calculate emp salary
 			int salary = emp_Rate_Per_Hour*emp_Hour;
 			
+			// update salary
 			total_Salary += salary;
 		}
-	
+		// print salary
 		System.out.println("Monthly salary of employee is : " + total_Salary);
        
 		
