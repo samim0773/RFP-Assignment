d00e214b85a3a0f36cb0efa8f726a88f6d2d0db0
Samim Aktar <samimaktar0773@gmail.com>
2021-10-10 23:48:10 +0530

    Add Comment

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,7 @@ package sample;
 
 public class EmployeeWage {
 	
+	// global constant variable 
 	public static final int is_Part_Time = 1;
 	public static final int is_Full_Time = 2;
 	public static final int emp_Rate_Per_Hour = 20;
@@ -11,26 +12,45 @@ public class EmployeeWage {
 		
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		
+		// check total emp hour is less than 100 and total working day less than 20
 		while( totalEmpHrs<=max_Hour_In_Month && totalWorkingDays  < num_Of_Working_Days ){
+			
+			// increment total working day
 			totalWorkingDays++;
 			
+			// generat random number 0 1 2
 			int empCheck = (int) Math.floor(Math.random()* 10)  % 3;
 			
+			// check condition 
 			switch (empCheck) {
+			
+				// full time = 2
 				case is_Full_Time:
 					empHrs = 8;
 					break;
+					
+				// part time = 1
 				case is_Part_Time:
 					empHrs = 4;
 					break;
+					
+				// absent = 0
 				default:
 					empHrs = 0;
 			}
+			
+			// calculate total emp hour
 			totalEmpHrs += empHrs;
+			
+			// print total working day
 			System.out.println("Day :" + totalWorkingDays + " Emp Hr: " + empHrs);
 			
 		}
+		
+		// calculate emp wage
 		int totalEmpWage = totalEmpHrs * emp_Rate_Per_Hour; 
+		
+		// print emp wage
 		System.out.println("Total Emp Wage :" + totalEmpWage);
        
 		
