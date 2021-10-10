377d6a1a338bbf216dba3e4724ef64c2bac29ea8
Samim Aktar <samimaktar0773@gmail.com>
2021-10-10 23:33:06 +0530

    Add comment

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,21 +2,24 @@ package sample;
 
 public class EmployeeWage {	
 	public static void main(String[] args) {
+		
+		// initialize variable and assign value
 		int Is_Full_Time = 1;
 		int Is_Part_Time = 2;
 		int Emp_Rate_Per_Hour = 20;
 		int emp_Hour = 0;
 		
 		//take the random value
-		 
 		double checkEmp = Math.floor(Math.random()*10)%3;
-//		System.out.println("CheckEmp: "+checkEmp);
+		
+		// System.out.println("CheckEmp: "+checkEmp);
 
 		//check the condition emp is present or not 
 		if(checkEmp == Is_Full_Time) {
 			emp_Hour = 8;
 			System.out.println("Employee is Full time");
 		}
+		// part time 2
 		else if ( checkEmp == Is_Part_Time) {
 			emp_Hour=4;
 			System.out.println("Employee is Part Time");
@@ -24,7 +27,10 @@ public class EmployeeWage {
 		
 		else
 			System.out.println("Emplyee is Absent");
+		// calculate emp wage
 		int empWage = emp_Hour * Emp_Rate_Per_Hour;
+		
+		// print emp wage
 		System.out.println("Employee Wage is : " +empWage);	
 	}
 
